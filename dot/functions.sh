a4e98a5a091438192327f16554cf72614829d440
Binar Nugroho <binarnugroho775@gmail.com>
2025-11-08 14:15:45 +0700

    Update configs

--- dot/functions.sh
+++ dot/functions.sh
@@ -7,3 +7,17 @@ function change_wallpaper() {
   hyprctl hyprpaper preload "$wp_path" >/dev/null 2>&1
   hyprctl hyprpaper wallpaper eDP-1,"$wp_path" >/dev/null 2>&1
 }
+
+nvim() {
+  if ! pidof socat >/dev/null 2>&1 && [ -n "$WSL_INTEROP" ]; then
+    [ -e /tmp/discord-ipc-0 ] && rm -f /tmp/discord-ipc-0
+    socat UNIX-LISTEN:/tmp/discord-ipc-0,fork \
+      EXEC:"npiperelay.exe //./pipe/discord-ipc-0" 2>/dev/null &
+  fi
+
+  if [ $# -eq 0 ]; then
+    command nvim
+  else
+    command nvim "$@"
+  fi
+}
