f98404a504a7726245bc1ac2acfd43d5f3f5d0bc
Binar Nugroho <binarnugroho775@gmail.com>
2025-11-21 21:13:02 +0700

    Update configs

--- config/hypr/scripts/swap-workspace.sh
+++ config/hypr/scripts/swap-workspace.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
 
-hyprctl dispatch hyprexpo:expo toggle
-sleep 0.26
+hyprctl dispatch hyprexpo:expo toggle || true
+sleep 0.30
 hyprctl dispatch workspace "$([ -n "$1" ] && printf "e-1" || printf "e+1")"
